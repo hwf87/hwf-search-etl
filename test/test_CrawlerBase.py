fe004c3458da4afe54f654cb2d0078349eca60ce
jackyfu <jackyfu1995@gmail.com>
2023-04-20 16:49:22 +0800

    add test cases

--- test/test_CrawlerBase.py
+++ test/test_CrawlerBase.py
@@ -5,52 +5,86 @@ sys.path.append("..")
 from src.CrawlerBase import ExtractorBase, TransformerBase, LoaderBase
 
 
+class ExtractorBaseConcrete(ExtractorBase):
+    def __init__(self):
+        super().__init__()
+
+    def extract(self):
+        pass
+
+
+class TransformerBaseConcrete(TransformerBase):
+    def __init__(self):
+        super().__init__()
+
+    def transform(self):
+        pass
+
+
+class LoaderBaseConcrete(LoaderBase):
+    def __init__(self):
+        super().__init__()
+
+    def transform(self):
+        pass
+
+
 class Test_ExtractorBase:
-    def test_bs4_parser(self):
+    @pytest.mark.parametrize(
+        "url, expect",
+        [
+            ("https://www.google.com", "Google"),
+            ("https://www.houzz.com", "Houzz"),
+            ("https://www.ted.com/talks", "TED Talks"),
+        ],
+    )
+    def test_bs4_parser(self, url, expect):
         """ """
-        ExtractorBase()
-        assert 1 == 1
+        EBC = ExtractorBaseConcrete()
+        soup = EBC.bs4_parser(url)
+        answer = soup.head.title.text
+        assert expect in answer
 
     @pytest.mark.parametrize("", [])
     def test_chunks(self):
         """ """
-        ExtractorBase()
+        ExtractorBaseConcrete()
 
     @pytest.mark.parametrize("", [])
     def test_date_converter(self):
         """ """
-        ExtractorBase()
+        ExtractorBaseConcrete()
 
     @pytest.mark.parametrize("", [])
     def test_multi_thread_process(self):
         """ """
-        ExtractorBase()
+        ExtractorBaseConcrete()
 
     @pytest.mark.parametrize("", [])
     def test_consume_jobs(self):
         """ """
-        ExtractorBase()
+        ExtractorBaseConcrete()
 
 
 class Test_TransformerBase:
     @pytest.mark.parametrize("", [])
     def test_chunks(self):
         """ """
-        TransformerBase()
+        TransformerBaseConcrete()
 
 
 class Test_LoaderBase:
     @pytest.mark.parametrize("", [])
     def test_get_es_client(self):
         """ """
-        LoaderBase()
+        LoaderBaseConcrete()
 
     @pytest.mark.parametrize("", [])
     def test_check_index(self):
         """ """
-        LoaderBase()
+        LoaderBaseConcrete()
 
     @pytest.mark.parametrize("", [])
     def test_create_index(self):
         """ """
-        LoaderBase()
+        LoaderBaseConcrete()
