b6d2d8a030b890efeeb2d1f001a889947af2fc2e
jackyfu <jackyfu1995@gmail.com>
2023-04-29 07:12:29 +0800

    add test cases for LoaderBase

--- test/test_data_loader.py
+++ test/test_data_loader.py
@@ -1,6 +1,8 @@
 import sys
 import pytest
 
+# from typing import Any, List, Dict
+
 sys.path.append("..")
 from src.houzz.houzz_data_loader import HouzzLoader
 from src.news.news_data_loader import NewsLoader
@@ -8,15 +10,76 @@ from src.tedtalk.tedtalk_data_loader import TedtalkLoader
 
 
 class Test_HouzzLoader:
+    @pytest.mark.parametrize("", [])
     def test_load(self):
         """ """
         HouzzLoader()
-        assert 1 == 1
 
-    @pytest.mark.parametrize("", [])
+    # @pytest.mark.parametrize("", [])
     def test_load_action_batch(self):
         """ """
-        HouzzLoader()
+        # expect: List[Dict[Any, Any]]
+        HL = HouzzLoader()
+        op_type = "index"
+        index_name = "mock_test"
+        documents = [
+            {
+                "uid": "item-1234",
+                "title": "Sample Item",
+                "details": "This is a sample item",
+                "posted": "2023-04-15",
+                "tags": ["fastapi", "python", "web development"],
+                "link": "https://example.com/sample-item",
+                "highlight": {},
+            },
+            {
+                "uid": "item-5678",
+                "title": "Sample Item 2",
+                "details": "This is a sample item",
+                "posted": "2023-04-15",
+                "tags": ["fastapi", "python", "web development"],
+                "link": "https://example.com/sample-item",
+                "highlight": {},
+            },
+        ]
+
+        expect = [
+            {
+                "_op_type": "index",
+                "_index": "mock_test",
+                "_id": "item-1234",
+                "_source": {
+                    "uid": "item-1234",
+                    "title": "Sample Item",
+                    "details": "This is a sample item",
+                    "posted": "2023-04-15",
+                    "tags": ["fastapi", "python", "web development"],
+                    "link": "https://example.com/sample-item",
+                    "highlight": {},
+                },
+            },
+            {
+                "_op_type": "index",
+                "_index": "mock_test",
+                "_id": "item-5678",
+                "_source": {
+                    "uid": "item-5678",
+                    "title": "Sample Item 2",
+                    "details": "This is a sample item",
+                    "posted": "2023-04-15",
+                    "tags": ["fastapi", "python", "web development"],
+                    "link": "https://example.com/sample-item",
+                    "highlight": {},
+                },
+            },
+        ]
+
+        answer = HL.load_action_batch(
+            op_type=op_type, index_name=index_name, documents=documents
+        )
+        answer = [ans for ans in answer]
+
+        assert answer == expect
 
 
 class Test_NewsLoader:
@@ -25,10 +88,71 @@ class Test_NewsLoader:
         """ """
         NewsLoader()
 
-    @pytest.mark.parametrize("", [])
+    # @pytest.mark.parametrize("", [])
     def test_load_action_batch(self):
         """ """
-        NewsLoader()
+        # expect: List[Dict[Any, Any]]
+        NL = NewsLoader()
+        op_type = "index"
+        index_name = "mock_test"
+        documents = [
+            {
+                "uid": "item-1234",
+                "title": "Sample Item",
+                "details": "This is a sample item",
+                "posted": "2023-04-15",
+                "tags": ["fastapi", "python", "web development"],
+                "link": "https://example.com/sample-item",
+                "highlight": {},
+            },
+            {
+                "uid": "item-5678",
+                "title": "Sample Item 2",
+                "details": "This is a sample item",
+                "posted": "2023-04-15",
+                "tags": ["fastapi", "python", "web development"],
+                "link": "https://example.com/sample-item",
+                "highlight": {},
+            },
+        ]
+
+        expect = [
+            {
+                "_op_type": "index",
+                "_index": "mock_test",
+                "_id": "item-1234",
+                "_source": {
+                    "uid": "item-1234",
+                    "title": "Sample Item",
+                    "details": "This is a sample item",
+                    "posted": "2023-04-15",
+                    "tags": ["fastapi", "python", "web development"],
+                    "link": "https://example.com/sample-item",
+                    "highlight": {},
+                },
+            },
+            {
+                "_op_type": "index",
+                "_index": "mock_test",
+                "_id": "item-5678",
+                "_source": {
+                    "uid": "item-5678",
+                    "title": "Sample Item 2",
+                    "details": "This is a sample item",
+                    "posted": "2023-04-15",
+                    "tags": ["fastapi", "python", "web development"],
+                    "link": "https://example.com/sample-item",
+                    "highlight": {},
+                },
+            },
+        ]
+
+        answer = NL.load_action_batch(
+            op_type=op_type, index_name=index_name, documents=documents
+        )
+        answer = [ans for ans in answer]
+
+        assert answer == expect
 
 
 class Test_TedtalkLoader:
@@ -37,7 +161,68 @@ class Test_TedtalkLoader:
         """ """
         TedtalkLoader()
 
-    @pytest.mark.parametrize("", [])
+    # @pytest.mark.parametrize("", [])
     def test_load_action_batch(self):
         """ """
-        TedtalkLoader()
+        # expect: List[Dict[Any, Any]]
+        TL = TedtalkLoader()
+        op_type = "index"
+        index_name = "mock_test"
+        documents = [
+            {
+                "uid": "item-1234",
+                "title": "Sample Item",
+                "details": "This is a sample item",
+                "posted": "2023-04-15",
+                "tags": ["fastapi", "python", "web development"],
+                "link": "https://example.com/sample-item",
+                "highlight": {},
+            },
+            {
+                "uid": "item-5678",
+                "title": "Sample Item 2",
+                "details": "This is a sample item",
+                "posted": "2023-04-15",
+                "tags": ["fastapi", "python", "web development"],
+                "link": "https://example.com/sample-item",
+                "highlight": {},
+            },
+        ]
+
+        expect = [
+            {
+                "_op_type": "index",
+                "_index": "mock_test",
+                "_id": "item-1234",
+                "_source": {
+                    "uid": "item-1234",
+                    "title": "Sample Item",
+                    "details": "This is a sample item",
+                    "posted": "2023-04-15",
+                    "tags": ["fastapi", "python", "web development"],
+                    "link": "https://example.com/sample-item",
+                    "highlight": {},
+                },
+            },
+            {
+                "_op_type": "index",
+                "_index": "mock_test",
+                "_id": "item-5678",
+                "_source": {
+                    "uid": "item-5678",
+                    "title": "Sample Item 2",
+                    "details": "This is a sample item",
+                    "posted": "2023-04-15",
+                    "tags": ["fastapi", "python", "web development"],
+                    "link": "https://example.com/sample-item",
+                    "highlight": {},
+                },
+            },
+        ]
+
+        answer = TL.load_action_batch(
+            op_type=op_type, index_name=index_name, documents=documents
+        )
+        answer = [ans for ans in answer]
+
+        assert answer == expect
