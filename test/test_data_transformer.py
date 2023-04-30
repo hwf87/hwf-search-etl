7f3bcf7f49185bf6a58200dfbef327c39eda7da0
jackyfu <jackyfu1995@gmail.com>
2023-04-30 22:19:33 +0800

    add test cases for transformer

--- test/test_data_transformer.py
+++ test/test_data_transformer.py
@@ -1,10 +1,18 @@
 import sys
+import json
 import pytest
+from typing import List
 
 sys.path.append("..")
 from src.houzz.houzz_data_transformer import HouzzTransformer
 from src.news.news_data_transformer import NewsTransformer
 from src.tedtalk.tedtalk_data_transformer import TedtalkTransformer
+
+
+def read_json_data(path: str) -> json:
+    f = open(path)
+    json_data = json.load(f)
+    return json_data
 
 
 class Test_HouzzTransformer:
@@ -24,10 +32,23 @@ class Test_NewsTransformer:
         """ """
         NewsTransformer()
 
-    @pytest.mark.parametrize("", [])
-    def test_inference(self):
+    @pytest.mark.parametrize(
+        "multi_sentence, test_data_path",
+        [
+            (
+                ["Hello world", "Thank god is Friday!"],
+                "./test/test_data/helloworld_tgif_embedding.json",
+            )
+        ],
+    )
+    def test_inference(self, multi_sentence: List[str], test_data_path: str):
         """ """
-        NewsTransformer()
+        NT = NewsTransformer()
+        answer = NT.inference(batch_texts=multi_sentence)
+        expect = read_json_data(test_data_path)["embeddings"]
+
+        assert answer[0] == expect[0]
+        assert answer[1] == expect[1]
 
 
 class Test_TedtalkTransformer:
